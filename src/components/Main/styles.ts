a595f446b4da53846838b81bdc18abd495ee027b
Mateus Lopes <mateus.lopes_stormgroup@prestador.globo>
2025-01-30 00:08:41 -0300

    Choro: Adding styled-components configs

---
+++ src/components/Main/styles.ts
@@ -0,0 +1,15 @@
+'use client'
+
+import styles from 'styled-components'
+
+export const Wrapper = styles.main`
+  background-color: #06092b;
+  color: #fff;
+  height: 100%;
+  padding: 3rem;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
